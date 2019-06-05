fe49f5b0649a33aa31ca10efdb0f66de17f81920
Kenneth Choi <kennethichoi@gmail.com>
2019-06-04 21:39:52 -0700

    Complete RailsAuth v1

--- W4D3/RailsAuth/app/models/user.rb
+++ W4D3/RailsAuth/app/models/user.rb
@@ -11,38 +11,40 @@
 #
 
 class User < ApplicationRecord
+  attr_reader :password
+  
   validates :username, :session_token, presence: true
   validates :password_digest, presence: {message: "Password can't be blank"}
   validates :password, length: {minimum: 6, allow_nil: true}
   after_initialize :ensure_session_token
 
-
-
-  def self.find_by_credentials
-    username = self.username
-    password = self.password
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
   end
 
   def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
   end
 
   def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
   end
 
   def password=(pw)
+    self.password_digest = BCrypt::Password.create(pw)
   end
-
 
   private
 
   def ensure_session_token
-
+    self.session_token ||= self.class.generate_session_token
   end
-
-
-
-
-
-
-
 end
